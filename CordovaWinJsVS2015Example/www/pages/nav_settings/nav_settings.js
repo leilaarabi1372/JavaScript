33c0f0ff6e58fbb17711e3823230006399acc58f
wanhhochoi <whchoi50@nate.com>
2016-11-01 10:54:58 +0900

    초기 커밋.

---
+++ CordovaWinJsVS2015Example/www/pages/nav_settings/nav_settings.js
@@ -0,0 +1,50 @@
+﻿(function () {
+	"use strict";
+
+	WinJS.UI.Pages.define("./pages/nav_settings/nav_settings.html", {
+		// This function is called whenever a user navigates to this page. It
+		// populates the page elements with the app's data.
+		ready: function (element, options) {
+			// Initialize the page here.
+
+			//keeps effect button for split views after page change
+			MyClass.hoverSplitView('cmdSettings');
+
+			//back button
+			document.addEventListener("backbutton", MyClass.onBackKeyDown, false);
+
+			//Creates ToggleSwitch and apply label
+			var mySwitchControl = new WinJS.UI.ToggleSwitch(document.querySelector("#exampleSwitch"));
+			mySwitchControl.labelOn = "On";
+			mySwitchControl.labelOff = "Off";
+
+			//recover setting from localstorage
+			var valueSwitch = MyClass.readItem("valueSwitch");
+			if (valueSwitch != null && valueSwitch == "1") {
+				mySwitchControl.checked = true;
+			}
+			else {
+				mySwitchControl.checked = false;
+			}
+
+			// control event for switch
+			mySwitchControl.addEventListener('change', function () { saveSwitch(mySwitchControl); });
+
+			//enter animation effect
+			WinJS.UI.Animation.enterPage([[document.querySelector("header")], [document.querySelector("section")]], null);
+
+		}
+
+	});
+
+	function saveSwitch(mySwitchControl) {
+		if (mySwitchControl.checked == true) {
+			MyClass.saveItem("valueSwitch", "1");//save setting in localstorage (see index.js)
+		}
+		else {
+			MyClass.saveItem("valueSwitch", "0");//save setting in localstorage (see index.js)
+		}
+	};
+
+
+})();
---
+++ CordovaWinJsVS2015Example/www/scripts/index.js
@@ -0,0 +1,137 @@
+﻿// For info empty model view:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+
+(function () {
+	"use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+    	// pause/resume Cordova
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+
+    	//check network connection at start
+        MyClass.ctrlConnection();
+	};
+
+    function onPause() {
+    };
+
+    function onResume() {
+    };
+
+    WinJS.UI.processAll().done(function () {
+
+    	var splitView = document.querySelector(".splitView").winControl;
+    	new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
+
+    	WinJS.UI.enableAnimations();
+
+    	WinJS.Navigation.addEventListener("navigating", function (e) {
+    		var elem = document.getElementById("pagesContainerDiv");
+
+			//animation for exit page (see sub-pages for enter animation)
+    		WinJS.UI.Animation.exitPage(elem.children).then(function () {
+    			WinJS.Utilities.empty(elem);
+    			WinJS.UI.Pages.render(e.detail.location, elem)
+					.then(function () {
+					});
+    		});
+    	});
+
+    	//load home sub-page at start
+    	WinJS.Navigation.navigate("./pages/nav_home/nav_home.html").done(function () {
+    	});
+
+    	var obj_cmdHome = document.querySelector('#cmdHome');
+    	obj_cmdHome.addEventListener('click', function () {
+    		WinJS.Navigation.navigate("./pages/nav_home/nav_home.html");
+    		splitView.closePane();
+    	}, false);
+
+    	var obj_cmdInfo = document.querySelector('#cmdSettings');
+    	obj_cmdInfo.addEventListener('click', function () {
+    		if (WinJS.Navigation.location != "./pages/nav_settings/nav_settings.html") {//avoid reload same sub-page on click
+    			WinJS.Navigation.navigate("./pages/nav_settings/nav_settings.html"); //load sub-page
+    		}
+    		splitView.closePane();
+    	}, false);
+
+    	var obj_cmdInfo = document.querySelector('#cmdHelp');
+    	obj_cmdInfo.addEventListener('click', function () {
+    		if (WinJS.Navigation.location != "./pages/nav_help/nav_help.html") {//avoid reload same sub-page on click
+    			WinJS.Navigation.navigate("./pages/nav_help/nav_help.html"); //load sub-page
+    		}
+    		splitView.closePane();
+    	}, false);
+
+    });
+
+})();
+
+var MyClass = {};
+(function () {
+
+	this.readItem = function (keyinput) {
+
+		if (typeof (Storage) !== "undefined") {
+			return window.localStorage.getItem(keyinput);
+		} else {
+			return "Error"; //Storage is not supported on this device
+		}
+
+	};
+
+	this.saveItem = function (keyinput, valinput) {
+
+		if (typeof (Storage) !== "undefined") {
+			window.localStorage.setItem(keyinput, valinput);
+		} else {
+			this.naviteAlert('Error'); //Storage is not supported on this device
+		}
+
+	};
+
+	this.naviteAlert = function (messaggio) {
+		navigator.notification.alert(
+			messaggio, //message
+			this.onConfirm,
+			'Notice', //title
+			'OK'
+		);
+	};
+
+	onConfirm = function (button) {
+		//navigator.notification.confirm(button,yourfunction);
+	};
+
+	this.ctrlConnection = function () {
+		var networkState = navigator.network.connection.type;
+
+		if (networkState == Connection.NONE) {
+			this.naviteAlert('Network unavailable. Check your Internet connection.');
+		}
+	};
+
+	//handle the back button
+	this.onBackKeyDown = function (args) {
+		if (WinJS.Navigation.canGoBack == true) {
+			WinJS.Navigation.back(1).done( /* Your success and error handlers */);
+		}
+	};
+
+	var lastSplitViewCommandPressed;
+
+	//keep press effect button for splitview
+	this.hoverSplitView = function (idCommand) {
+		if (lastSplitViewCommandPressed != null) {
+			WinJS.Utilities.removeClass(document.getElementById(lastSplitViewCommandPressed), 'SplitViewCommand-hover');
+		}
+
+		WinJS.Utilities.addClass(document.getElementById(idCommand), 'SplitViewCommand-hover');
+
+		lastSplitViewCommandPressed = idCommand;
+	};
+
+}).apply(MyClass);
